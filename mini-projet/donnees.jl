6ddb5d931542a88113b948b5bc32e18976458d13
NadegeEv <nadege.polette@gmail.com>
2019-12-04 11:37:58 +0100

    ajout du mini projet

---
+++ mini-projet/donnees.jl
@@ -0,0 +1,27 @@
+# matrice des incidences I
+
+# 1 2 3 4 A B C D E F U
+I = [[0,0,0,0,0,1,1,0,0,0,0],
+     [0,0,0,0,1,1,0,0,0,0,0],
+     [0,0,0,0,1,0,0,0,1,0,0]
+     [0,0,0,0,0,0,0,0,1,1,0]
+     [0,-1,-1,0,0,0,0,1,0,0,0],
+     [-1,-1,0,0,0,0,0,1,0,0,0],
+     [-1,0,0,0,0,0,0,1,0,0,1],
+     [0,0,0,0,-1,-1,-1,0,-1,0,1],
+     [0,0,-1,-1,0,0,0,1,0,1,0],
+     [0,0,0,-1,0,0,0,0,-1,0,1],
+     [0,0,0,0,0,0,-1,-1,0,-1,0]]
+
+# matrice des coûts M
+M = [[Inf,Inf,Inf,Inf,Inf,14,19,Inf,Inf,Inf,Inf],
+     [Inf,Inf,Inf,Inf,9,10,Inf,Inf,Inf,Inf,Inf],
+     [Inf,Inf,Inf,Inf,12,Inf,Inf,Inf,18,Inf,Inf]
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,26,10,Inf]
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,25,Inf,Inf,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,11,Inf,Inf,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,8,Inf,Inf,14],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,15],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,4,Inf,7,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,20],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf]]
