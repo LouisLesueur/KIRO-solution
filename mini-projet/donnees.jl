3803c1026fe7b2dbe5fdcf1f9e3652457042d337
NadegeEv <nadege.polette@gmail.com>
2019-12-08 14:20:46 +0100

    ajout de toutes les matrices des données

--- mini-projet/donnees.jl
+++ mini-projet/donnees.jl
@@ -1,27 +1,62 @@
 # matrice des incidences I
 
-# 1 2 3 4 A B C D E F U
-I = [[0,0,0,0,0,1,1,0,0,0,0],
-     [0,0,0,0,1,1,0,0,0,0,0],
-     [0,0,0,0,1,0,0,0,1,0,0]
-     [0,0,0,0,0,0,0,0,1,1,0]
-     [0,-1,-1,0,0,0,0,1,0,0,0],
-     [-1,-1,0,0,0,0,0,1,0,0,0],
-     [-1,0,0,0,0,0,0,1,0,0,1],
-     [0,0,0,0,-1,-1,-1,0,-1,0,1],
-     [0,0,-1,-1,0,0,0,1,0,1,0],
-     [0,0,0,-1,0,0,0,0,-1,0,1],
-     [0,0,0,0,0,0,-1,-1,0,-1,0]]
+# ajout d'une super source qui dessert toutes les sources (S)
+# ajout d'une super usine pour imposer le flot de sortie (T)
+# 1 2 3 4 A B C D E F U : num ligne -> num colonne
+I = [[0,1,1,1,1,0,0,0,0,0,0,0,0],
+     [-1,0,0,0,0,0,1,1,0,0,0,0,0],
+     [-1,0,0,0,0,1,1,0,0,0,0,0,0],
+     [-1,0,0,0,0,1,0,0,0,1,0,0,0]
+     [-1,0,0,0,0,0,0,0,0,1,1,0,0]
+     [0,0,-1,-1,0,0,0,0,1,0,0,0,0],
+     [0,-1,-1,0,0,0,0,0,1,0,0,0,0],
+     [0,-1,0,0,0,0,0,0,1,0,0,1,0],
+     [0,0,0,0,0,-1,-1,-1,0,-1,0,1,0],
+     [0,0,0,-1,-1,0,0,0,1,0,1,0,0],
+     [0,0,0,0,-1,0,0,0,0,-1,0,1,0],
+     [0,0,0,0,0,0,0,-1,-1,0,-1,0,1],
+     [0,0,0,0,0,0,0,0,0,0,0,-1,0]]
 
 # matrice des coûts M
-M = [[Inf,Inf,Inf,Inf,Inf,14,19,Inf,Inf,Inf,Inf],
-     [Inf,Inf,Inf,Inf,9,10,Inf,Inf,Inf,Inf,Inf],
-     [Inf,Inf,Inf,Inf,12,Inf,Inf,Inf,18,Inf,Inf]
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,26,10,Inf]
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,25,Inf,Inf,Inf],
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,11,Inf,Inf,Inf],
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,8,Inf,Inf,14],
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,15],
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,4,Inf,7,Inf],
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,20],
-     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf]]
+M = [[Inf,0,0,0,0,Inf,Inf,Inf,Inf,Inf,Inf,Inf],
+     [0,Inf,Inf,Inf,Inf,Inf,14,19,Inf,Inf,Inf,Inf,Inf],
+     [0,Inf,Inf,Inf,Inf,9,10,Inf,Inf,Inf,Inf,Inf,Inf],
+     [0,Inf,Inf,Inf,Inf,12,Inf,Inf,Inf,18,Inf,Inf,Inf]
+     [0,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,26,10,Inf,Inf]
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,25,Inf,Inf,Inf,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,11,Inf,Inf,Inf,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,8,Inf,Inf,14,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,15,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,4,Inf,7,Inf,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,20,Inf],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,0],
+     [Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf,Inf]]
+
+# matrice du flot max supporté
+F_max = [[0,205,75,95,195,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,200,200,0,0,0,0,0],
+        [0,0,0,0,0,200,200,0,0,0,0,0,0],
+        [0,0,0,0,0,200,0,0,0,200,0,0,0]
+        [0,0,0,0,0,0,0,0,0,200,200,0,0]
+        [0,0,0,0,0,0,0,0,200,0,0,0,0],
+        [0,0,0,0,0,0,0,0,200,0,0,0,0],
+        [0,0,0,0,0,0,0,0,200,0,0,200,0],
+        [0,0,0,0,0,0,0,0,0,0,0,200,0],
+        [0,0,0,0,0,0,0,0,200,0,200,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,200,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,570],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+F_min = [[0,205,75,95,195,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0]
+        [0,0,0,0,0,0,0,0,0,0,0,0,0]
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,570],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0]]
